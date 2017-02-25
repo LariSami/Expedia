470fca42149e464e9047baab00924fdd28e15fd2
Larisa Sami <larisa.23@live.com>
2017-02-25 03:28:37 +0200

    Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,7 +34,6 @@ public class Main {
 
   public static void main(String[] args) 
   {
-	StringBuilder sb = new StringBuilder();
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
@@ -43,6 +42,7 @@ public class Main {
     	HttpURLConnection conn = null;
     	try 
     	{
+    		StringBuilder sb = new StringBuilder();
     		JFrame frame = new JFrame("jFrame");
     		String cityName = JOptionPane.showInputDialog(frame, "What's your destination city?  eg. Moscow");
     		URL url= null;
